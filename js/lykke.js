8306b2c69a32c4edf8f6261dd995450f22c111f8
Dmitriy Chudnyi <dmitriy@chudnyi.com>
2018-03-22 10:59:00 +0200

    [kript] lykke.js fetchMyTrades WIP

--- js/lykke.js
+++ js/lykke.js
@@ -21,6 +21,7 @@ module.exports = class lykke extends Exchange {
                 'fetchOpenOrders': true,
                 'fetchClosedOrders': true,
                 'fetchOrders': true,
+                'fetchMyTrades': true,
             },
             'requiredCredentials': {
                 'apiKey': true,
@@ -65,6 +66,7 @@ module.exports = class lykke extends Exchange {
                         'Orders',
                         'Orders/{id}',
                         'Wallets',
+                        'History/trades',
                     ],
                     'post': [
                         'Orders/limit',
@@ -180,8 +182,9 @@ module.exports = class lykke extends Exchange {
     parseTicker (ticker, market = undefined) {
         let timestamp = this.milliseconds ();
         let symbol = undefined;
-        if (market)
+        if (market) {
             symbol = market['symbol'];
+        }
         ticker = ticker['Result'];
         return {
             'symbol': symbol,
@@ -243,12 +246,15 @@ module.exports = class lykke extends Exchange {
         let status = this.parseOrderStatus (order['Status']);
         let symbol = undefined;
         if (!market) {
-            if ('AssetPairId' in order)
-                if (order['AssetPairId'] in this.markets_by_id)
+            if ('AssetPairId' in order) {
+                if (order['AssetPairId'] in this.markets_by_id) {
                     market = this.markets_by_id[order['AssetPairId']];
+                }
+            }
         }
-        if (market)
+        if (market) {
             symbol = market['symbol'];
+        }
         let timestamp = undefined;
         if ('LastMatchTime' in order) {
             timestamp = this.parse8601 (order['LastMatchTime']);
@@ -333,39 +339,205 @@ module.exports = class lykke extends Exchange {
                 orderbook['timestamp'] = Math.max (orderbook['timestamp'], timestamp);
             }
         }
-        if (!timestamp)
+        if (!timestamp) {
             timestamp = this.milliseconds ();
+        }
         return this.parseOrderBook (orderbook, orderbook['timestamp'], 'bids', 'asks', 'Price', 'Volume');
     }
 
     parseBidAsk (bidask, priceKey = 0, amountKey = 1) {
         let price = parseFloat (bidask[priceKey]);
         let amount = parseFloat (bidask[amountKey]);
-        if (amount < 0)
+        if (amount < 0) {
             amount = -amount;
-        return [ price, amount ];
+        }
+        return [price, amount];
     }
 
     sign (path, api = 'public', method = 'GET', params = {}, headers = undefined, body = undefined) {
         let url = this.urls['api'][api] + '/' + this.implodeParams (path, params);
         let query = this.omit (params, this.extractParams (path));
         if (api === 'public') {
-            if (Object.keys (query).length)
+            if (Object.keys (query).length) {
                 url += '?' + this.urlencode (query);
+            }
         } else if (api === 'private') {
-            if (method === 'GET')
-                if (Object.keys (query).length)
+            if (method === 'GET') {
+                if (Object.keys (query).length) {
                     url += '?' + this.urlencode (query);
+                }
+            }
             this.checkRequiredCredentials ();
             headers = {
                 'api-key': this.apiKey,
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
             };
-            if (method === 'POST')
-                if (Object.keys (params).length)
+            if (method === 'POST') {
+                if (Object.keys (params).length) {
                     body = this.json (params);
+                }
+            }
         }
         return { 'url': url, 'method': method, 'body': body, 'headers': headers };
     }
-};
+
+    async fetchMyTrades (symbol = undefined, since = undefined, limit = undefined, params = {}) {
+        await this.loadMarkets ();
+        let request = {
+            // 'type': 'all', // any position, closed position, closing position, no position
+            // 'trades': false, // whether or not to include trades related to position in output
+            // 'start': 1234567890, // starting unix timestamp or trade tx id of results (exclusive)
+            // 'end': 1234567890, // ending unix timestamp or trade tx id of results (inclusive)
+            // 'ofs' = result offset
+        };
+        if (typeof since !== 'undefined') {
+            request['start'] = parseInt (since / 1000);
+        }
+        let response = await this.privateGetHistoryTrades (this.extend (request, params));
+        console.log ('[privateGetHistorTtrades] response', response);
+
+        /*
+            let trades = response['result']['trades'];
+            let ids = Object.keys (trades);
+            for (let i = 0; i < ids.length; i++) {
+              trades[ids[i]]['id'] = ids[i];
+            }
+            return this.parseTrades (trades, undefined, since, limit);
+        */
+
+        return this.parseTrades (response, undefined, since, limit);
+    }
+
+    parseTrade (trade, market = undefined) {
+        /*
+            {
+              "Id": "string",
+              "DateTime": "2018-03-21T16:10:43.056Z",
+              "State": "InProgress",
+              "Amount": 0,
+              "Asset": "string",
+              "AssetPair": "string",
+              "Price": 0
+            }
+        */
+        let timestamp = this.parse8601 (trade['DateTime']);
+        let side = undefined;
+        let type = undefined;
+        let price = parseFloat (trade['Price']);
+        let amount = parseFloat (trade['Amount']);
+        let id = trade['Id'];
+        let order = undefined;
+        let fee = undefined;
+
+        let symbol = (market) ? market['symbol'] : undefined;
+
+        return {
+            'id': id,
+            'order': order,
+            'info': trade,
+            'timestamp': timestamp,
+            'datetime': this.iso8601 (timestamp),
+            'symbol': symbol,
+            'type': type,
+            'side': side,
+            'price': price,
+            'amount': amount,
+            'fee': fee,
+        };
+
+    }
+
+    parseTrade_bitfinex (trade, market) {
+        let timestamp = parseInt (parseFloat (trade['timestamp'])) * 1000;
+        let side = trade['type'].toLowerCase ();
+        let orderId = this.safeString (trade, 'order_id');
+        let price = parseFloat (trade['price']);
+        let amount = parseFloat (trade['amount']);
+        let cost = price * amount;
+        let fee = undefined;
+        if ('fee_amount' in trade) {
+            let feeCost = this.safeFloat (trade, 'fee_amount');
+            let feeCurrency = this.safeString (trade, 'fee_currency');
+            if (feeCurrency in this.currencies_by_id)
+                feeCurrency = this.currencies_by_id[feeCurrency]['code'];
+            fee = {
+                'cost': feeCost,
+                'currency': feeCurrency,
+            };
+        }
+        return {
+            'id': trade['tid'].toString (),
+            'info': trade,
+            'timestamp': timestamp,
+            'datetime': this.iso8601 (timestamp),
+            'symbol': market['symbol'],
+            'type': undefined,
+            'order': orderId,
+            'side': side,
+            'price': price,
+            'amount': amount,
+            'cost': cost,
+            'fee': fee,
+        };
+    }
+
+
+    parseTrade_kraken (trade, market = undefined) {
+        let timestamp = undefined;
+        let side = undefined;
+        let type = undefined;
+        let price = undefined;
+        let amount = undefined;
+        let id = undefined;
+        let order = undefined;
+        let fee = undefined;
+        if (!market) {
+            market = this.findMarketByAltnameOrId (trade['pair']);
+        }
+        if ('ordertxid' in trade) {
+            order = trade['ordertxid'];
+            id = trade['id'];
+            timestamp = parseInt (trade['time'] * 1000);
+            side = trade['type'];
+            type = trade['ordertype'];
+            price = parseFloat (trade['price']);
+            amount = parseFloat (trade['vol']);
+            if ('fee' in trade) {
+                let currency = undefined;
+                if (market) {
+                    currency = market['quote'];
+                }
+                fee = {
+                    'cost': parseFloat (trade['fee']),
+                    'currency': currency,
+                };
+            }
+        } else {
+            timestamp = parseInt (trade[2] * 1000);
+            side = (trade[3] === 's') ? 'sell' : 'buy';
+            type = (trade[4] === 'l') ? 'limit' : 'market';
+            price = parseFloat (trade[0]);
+            amount = parseFloat (trade[1]);
+            let tradeLength = trade.length;
+            if (tradeLength > 6) {
+                id = trade[6];
+            } // artificially added as per #1794
+        }
+        let symbol = (market) ? market['symbol'] : undefined;
+        return {
+            'id': id,
+            'order': order,
+            'info': trade,
+            'timestamp': timestamp,
+            'datetime': this.iso8601 (timestamp),
+            'symbol': symbol,
+            'type': type,
+            'side': side,
+            'price': price,
+            'amount': amount,
+            'fee': fee,
+        };
+    }
+
+}
